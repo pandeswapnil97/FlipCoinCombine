045a4c2fcabcba4b6062f4cc3c604a65519f4d48
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-05 14:40:23 +0530

    Flipping a Coin to Display Heads or Tails as winner

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,18 @@
 #!/bin/bash -x
 
 echo "Welcome To Flip Coin Combination Program"
+
+#Use Case 1 (Flipping a Coin to Display Heads or Tails as winner)
+
+flip_check=$((RANDOM%2))
+
+isHeads=1
+
+echo "Flipping Coin Value: "$flip_check
+
+if [ $flip_check -eq $isHeads ]
+then
+	echo "So, Heads is Winner"
+else
+	echo "So, Tails is Winner"
+fi
